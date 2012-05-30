75d8b02106618c0cbbf2425b663333dc93ac7e6c
Jan Keromnes <janx@linux.com>
2012-05-30 13:26:26 -0700

    [CodeMirror] enable line wrapping

--- js/mirror.js
+++ js/mirror.js
@@ -1,6 +1,7 @@
 var editor = CodeMirror(document.body, {
   value: "function myScript(){return 100;}\n",
   mode:  "javascript",
-  lineNumbers: true
+  lineNumbers: true,
+  lineWrapping: true
 });
 
