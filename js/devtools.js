cf557e02c72c9c108fc8f01545f853c7a1e173ff
Jan Keromnes <janx@linux.com>
2012-06-13 21:17:01 -0700

    supported resources types: javascript, css

--- js/devtools.js
+++ js/devtools.js
@@ -1,7 +1,9 @@
+// watch when resource contents are committed
 chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(resource, content) {
   console.log('resource content committed', resource, content);
 });
 
+// create the CodeMirror panel
 chrome.devtools.panels.create('CodeMirror', 'img/mirror32.png', 'index.html', function(panel) {
   console.log('panel',JSON.stringify(panel),panel);
 
@@ -9,17 +11,20 @@ chrome.devtools.panels.create('CodeMirror', 'img/mirror32.png', 'index.html', fu
       editor = null,
       buffer = null;
 
-  function load(content, line) {
+  // load resource code into the editor
+  function load(content, type, line) {
     if (editor) {
-      console.log('loading', content, line);
+      console.log('loading', content, type, line);
       editor.setValue(content);
+      editor.setOption('mode', (type === 'script' ? 'javascript' : 'css'));
       editor.setCursor({line:line||0, ch:0});
     } else {
-      buffer = {content:content, line:line};
+      buffer = {content:content, type:type, line:line};
       console.log('buffering load', buffer);
     }
   }
 
+  // commit changes made to resource code
   function save() {
     if (editor) {
       console.log('saving', editor.getValue());
@@ -30,18 +35,20 @@ chrome.devtools.panels.create('CodeMirror', 'img/mirror32.png', 'index.html', fu
     }
   }
 
+  // use CodeMirror panel to open resources
   chrome.devtools.panels.setOpenResourceHandler(function(resource, line) {
-    console.log('open resource', resource, line);
+    console.log('open resource', resource, resource.url, resource.type, line);
 
     res = resource;
     res.getContent(function(content, encoding) {
       console.log('encoding', encoding);
-      load(content, line);
+      load(content, res.type, line);
     });
 
     panel.show();
   });
 
+  // as panels load lazily, grab the editor when it's ready
   panel.onShown.addListener(function(window) {
     if (!editor) {
       console.log('setting editor', window.editor);
@@ -54,6 +61,7 @@ chrome.devtools.panels.create('CodeMirror', 'img/mirror32.png', 'index.html', fu
     }
   });
 
+  // wire WebInspector search bar to the editor
   panel.onSearch.addListener(function(action, query) {
     console.log('search',action,query);
     if (editor) {
@@ -65,6 +73,7 @@ chrome.devtools.panels.create('CodeMirror', 'img/mirror32.png', 'index.html', fu
   //var buttonsave = panel.createStatusBarButton('img/mirror16.png', 'Save', false);
   //buttonsave.onClicked.addListener(save);
 
+  // test inspectedWindow.getResources
   var buttonres = panel.createStatusBarButton('img/mirror16.png', 'Resources', false);
   buttonres.onClicked.addListener(function() {
     chrome.devtools.inspectedWindow.getResources(function(res){
