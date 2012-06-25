28c2d3c65d567cbbe200d08d3c83f40871dcc8cd
Jan Keromnes <janx@linux.com>
2012-06-25 14:11:38 -0700

    breakpoints UI

--- js/mirror.js
+++ js/mirror.js
@@ -2,5 +2,13 @@ var editor = CodeMirror(document.body, {
   value: '',
   mode:  'javascript',
   lineNumbers: true,
-  lineWrapping: true
+  lineWrapping: true,
+  onGutterClick: function(cm, line) {
+    var info = cm.lineInfo(line);
+    if (info.markerText) {
+      cm.clearMarker(line);
+    } else {
+      cm.setMarker(line, "<span style=\"color: #0AF; font-weight: bold;\">%N%</span>");
+    }
+  }
 });
